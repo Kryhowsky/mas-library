575c39a40fdc7055a099d3b680e32041ef636188
Kryhowsky <piter71-1997@go2.pl>
2022-04-13 20:49:10 +0200

    Initial commit

---
+++ src/main/java/com/kryhowsky/maslibrary/MasLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.kryhowsky.maslibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kryhowsky/maslibrary/MasLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kryhowsky.maslibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
